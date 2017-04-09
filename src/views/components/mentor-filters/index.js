5dea43b952877420b6a5575f93a048e0681cffa3
buzzwin <gunjanv@yahoo.com>
2017-04-09 16:25:10 -0500

    Fixed Filters and Sign In Buttons

--- src/views/components/mentor-filters/index.js
+++ src/views/components/mentor-filters/index.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router';
 function MentorFilters({filter}) {
   return (
     <ul className="task-filters">
-      <li><Link activeClassName="active" to={{pathname: '/', query: {filter: 'completed'}}}>Recommendations</Link></li>
+
       <li><Link className={classNames({active: !filter})} to="/">View All</Link></li>
-      <li><Link activeClassName="active" to={{pathname: '/', query: {filter: 'active'}}}>Saved </Link></li>
+        <li><Link activeClassName="active" to={{pathname: '/', query: {filter: 'completed'}}}>Favorites</Link></li>
     </ul>
   );
 }
